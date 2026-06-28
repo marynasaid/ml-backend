89878de404b60a8c18a8de75e87a96d7848fcf72
sofia <maryna.said@gmail.com>
2026-06-28 12:48:10 +0200

    fix cors

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# ✅ CORS (правильная версия для Flutter Web)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # загрузка модели
 model = joblib.load("catboost_model.pkl")
