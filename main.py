4cab6835b48b4815826ef9b6c92dc8e9072cb917
sofia <maryna.said@gmail.com>
2026-06-28 11:45:12 +0200

    initial backend setup

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+# загрузка модели
+model = joblib.load("catboost_model.pkl")
+
+
+class InputData(BaseModel):
+    is_flow: int
+    is_first_flow: int
+    last_cycle_length: float
+    cycle_progress: float
+    is_last_flow: int
+    flow_length: float
+    cycle_length_deviation: float
+    avg_cycle_length: float
+
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
+
+@app.post("/predict")
+def predict(data: InputData):
+
+    x = np.array([[
+        data.is_flow,
+        data.is_first_flow,
+        data.last_cycle_length,
+        data.cycle_progress,
+        data.is_last_flow,
+        data.flow_length,
+        data.cycle_length_deviation,
+        data.avg_cycle_length
+    ]])
+
+    pred = model.predict(x)[0]
+
+    return {"prediction": float(pred)}
